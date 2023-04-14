801996eb54e1e9ea3bbdbc14db356acf40d051b4
Ju-Min-Kyung <alsrud7767@naver.com>
2023-04-14 19:05:48 +0900

    BCSD homework2

--- koreatechgram/koreatechgram/ContentView.swift
+++ koreatechgram/koreatechgram/ContentView.swift
@@ -37,10 +37,19 @@ struct ContentView: View {
             
                 
             Image("profileImage")
-                .resizable()
-                .scaledToFit()
+                .resizable().aspectRatio(1, contentMode: .fit)
             
             HStack(spacing: 0){
+                
+                HStack(spacing: 8){
+                    Image(systemName: "heart").resizable().frame(width: 20, height: 20)
+                    Image(systemName: "message")
+                        .resizable().frame(width: 20, height: 20)
+                    Image(systemName: "location").resizable().frame(width: 20, height: 20)
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading).padding(16)
+                
+                Image(systemName: "bookmark").resizable().frame(width: 20, height: 20, alignment: .trailing).padding(16)
                 //과제
                 //4개의 아이콘, 3개는 왼쪽, 1개는 오른쪽
                 //양쪽에 16씩 padding, icon 사이마다 8씩 패딩 넣기 - for문..?
